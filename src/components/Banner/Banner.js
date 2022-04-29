8c8813b16c760ccd9f5f3f762c94ff60bb952b1f
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-04-30 02:07:35 +0600

    banner section recustomized

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,39 +3,69 @@ import './Banner.css';
 
 const Banner = () => {
   return (
-    <section class="bg-gray-50">
-      <div class="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-        <div class="max-w-xl mx-auto text-center">
-          <h1 class="text-3xl font-extrabold sm:text-5xl">
-            Understand User Flow.
-            <strong class="font-extrabold text-red-700 sm:block">
-              Increase Conversion.
-            </strong>
-          </h1>
-
-          <p class="mt-4 sm:leading-relaxed sm:text-xl">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
-          </p>
-
-          <div class="flex flex-wrap justify-center gap-4 mt-8">
-            <a
-              class="block w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring"
-              href="/get-started"
-            >
-              Get Started
-            </a>
-
-            <a
-              class="block w-full px-12 py-3 text-sm font-medium text-red-600 rounded shadow sm:w-auto hover:text-red-700 active:text-red-500 focus:outline-none focus:ring"
-              href="/about"
-            >
-              Learn More
-            </a>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-14">
+      <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+        <div className="flex flex-col justify-center">
+          <div className="max-w-xl mb-6">
+            <div>
+              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full text-neutral-100 bg-teal-400">
+                Track Inventory
+              </p>
+            </div>
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Product Inventory
+              <br className="hidden md:block" />
+              <span className="inline-block text-purple-600">Management</span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Every company needs an inventory management system. Whether you’re
+              in wholesale, retail, service or virtually any other industry, you
+              have to keep track of your products.
+            </p>
           </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn How it Works
+            <svg
+              className="inline-block w-3 ml-2"
+              fill="currentColor"
+              viewBox="0 0 12 12"
+            >
+              <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+            </svg>
+          </a>
+        </div>
+        <div className="relative">
+          <svg
+            className="absolute w-full text-teal-400"
+            fill="currentColor"
+            viewBox="0 0 600 392"
+          >
+            <rect x="0" y="211" width="75" height="181" rx="8" />
+            <rect x="525" y="260" width="75" height="132" rx="8" />
+            <rect x="105" y="83" width="75" height="309" rx="8" />
+            <rect x="210" y="155" width="75" height="237" rx="8" />
+            <rect x="420" y="129" width="75" height="263" rx="8" />
+            <rect x="315" y="0" width="75" height="392" rx="8" />
+          </svg>
+          <svg
+            className="relative w-full text-violet-600"
+            fill="currentColor"
+            viewBox="0 0 600 392"
+          >
+            <rect x="0" y="311" width="75" height="81" rx="8" />
+            <rect x="525" y="351" width="75" height="41" rx="8" />
+            <rect x="105" y="176" width="75" height="216" rx="8" />
+            <rect x="210" y="237" width="75" height="155" rx="8" />
+            <rect x="420" y="205" width="75" height="187" rx="8" />
+            <rect x="315" y="83" width="75" height="309" rx="8" />
+          </svg>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
