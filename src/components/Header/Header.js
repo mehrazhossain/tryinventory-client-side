00cb39f0034189b0f43f810d6496ebeb5d18084b
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-04-29 10:30:17 +0600

    Initial setup & configured tailwind

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">
+        Hello World! <br /> From Header Component.
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
