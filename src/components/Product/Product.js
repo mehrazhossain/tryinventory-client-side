92af77b745354a189c484c21675d9277c742c197
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-04 02:24:40 +0600

    fetch jsonplaceholder data for inventory product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({ photo }) => {
+  const { id, title, url } = photo;
   return (
     <div>
-      <h2 className="text-6xl text-center">Inventory Product</h2>
+      <a
+        href="/"
+        aria-label="View Item"
+        className="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2"
+      >
+        <div className="flex flex-col h-full">
+          <img src={url} alt="" />
+          <div className="flex-grow border border-t-0 rounded-b">
+            <div className="p-5">
+              <h6 className="mb-2 font-semibold leading-5">{title}</h6>
+              <p className="text-sm text-gray-900">
+                Sportacus andrew weatherall goose Refined gentlemen super mario
+                des lynam alpha trion zap rowsdower.
+              </p>
+            </div>
+          </div>
+        </div>
+      </a>
     </div>
   );
 };
