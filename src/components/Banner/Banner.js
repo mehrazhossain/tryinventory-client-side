ed5f27be5f5a1c411409f3eb732881a1efb02a70
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-08 05:48:13 +0600

    remove extra margin & padding

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css';
 
 const Banner = () => {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-14">
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-8 lg:pb-8">
       <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
         <div className="flex flex-col justify-center">
           <div className="max-w-xl mb-6">
